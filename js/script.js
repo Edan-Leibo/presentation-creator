c0c0eb982b152fea3275898ffc0882fc0e0d95f6
Edan <edan.leibo@gmail.com>
2019-04-07 10:12:46 +0300

    create basic structure - no buttons

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $('.slick-carousel').slick({
+        infinite: false,
+        speed: 1000,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: false,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+});
